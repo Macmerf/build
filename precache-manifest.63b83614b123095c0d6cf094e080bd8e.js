517de09c468f9a79d0af9c45efe7bdafb0f5b961
Damir <nuriakhmetovd.t1998@gmail.com>
2020-08-30 16:03:37 +0300

    first commit

---
+++ precache-manifest.63b83614b123095c0d6cf094e080bd8e.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "075170232ed4da5bca86c3318718ad26",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8d1fe1e14373c58fb33c",
+    "url": "./static/css/main.a9545264.chunk.css"
+  },
+  {
+    "revision": "637a19bf5fcd00db502b",
+    "url": "./static/js/2.209c3ad8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.209c3ad8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d1fe1e14373c58fb33c",
+    "url": "./static/js/main.382a8f0e.chunk.js"
+  },
+  {
+    "revision": "ca8051aa2fe5b3ef84c3",
+    "url": "./static/js/runtime-main.673723db.js"
+  },
+  {
+    "revision": "2e93e1982315035f647a035f039329d6",
+    "url": "./static/media/2-layers.2e93e198.svg"
+  },
+  {
+    "revision": "44bde8744ccc0de7995a23b0e82dd281",
+    "url": "./static/media/Exo2.0-Thin.44bde874.otf"
+  },
+  {
+    "revision": "9a713fab69c1060ba639dff59d4a4a50",
+    "url": "./static/media/Pattern.9a713fab.png"
+  },
+  {
+    "revision": "a1a67b20f137ee2b13886a259c232c63",
+    "url": "./static/media/Photo.a1a67b20.png"
+  },
+  {
+    "revision": "150ccf41411b1178837b1e5769855b9a",
+    "url": "./static/media/Trebuchet-BoldItalic.150ccf41.woff"
+  },
+  {
+    "revision": "b66de6f45da6ff2de1eb0b377a16aa0a",
+    "url": "./static/media/Trebuchet-BoldItalic.b66de6f4.ttf"
+  },
+  {
+    "revision": "d6d6a1dc21aa28051d0795f5651f0c5e",
+    "url": "./static/media/Trebuchet-BoldItalic.d6d6a1dc.eot"
+  },
+  {
+    "revision": "7afeaf2b8e5fee8b3332318a9f216092",
+    "url": "./static/media/TrebuchetMS-Bold.7afeaf2b.ttf"
+  },
+  {
+    "revision": "9b45a5e6fd7e43502d74c1cf2bb77ded",
+    "url": "./static/media/TrebuchetMS-Bold.9b45a5e6.woff"
+  },
+  {
+    "revision": "ca7ac163de240c9e7d6fc018ad0f4dad",
+    "url": "./static/media/TrebuchetMS-Bold.ca7ac163.eot"
+  },
+  {
+    "revision": "4066599b964d6dd3197730d06b44d09a",
+    "url": "./static/media/TrebuchetMS-Italic.4066599b.eot"
+  },
+  {
+    "revision": "7606ea3aa903e37d2f1f80c224994e42",
+    "url": "./static/media/TrebuchetMS-Italic.7606ea3a.woff"
+  },
+  {
+    "revision": "9c3439b0fac978a94c074c21abccdea3",
+    "url": "./static/media/TrebuchetMS-Italic.9c3439b0.ttf"
+  },
+  {
+    "revision": "34723a2fbef5882977e8392d020956af",
+    "url": "./static/media/TrebuchetMS.34723a2f.woff"
+  },
+  {
+    "revision": "530d3166bc7f3ee970eec68df2a2bd2e",
+    "url": "./static/media/TrebuchetMS.530d3166.ttf"
+  },
+  {
+    "revision": "be77a7db37f37471cff8616dc5b875c5",
+    "url": "./static/media/TrebuchetMS.be77a7db.eot"
+  },
+  {
+    "revision": "d05f53afee9c00feedb3b4aa028d7592",
+    "url": "./static/media/default.d05f53af.svg"
+  },
+  {
+    "revision": "097d5f821823d9c08c3119ac58bd5d6b",
+    "url": "./static/media/disabled.097d5f82.svg"
+  }
+]);
